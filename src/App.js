7636ae45570dcf64582c519279d05180b3eb7f88
Carlos-Jr1 <junior_kd2012@hotmail.com>
2022-04-25 23:45:22 -0300

    input-carlos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import Forms from './componentes/Forms';
 
 function App() {
   return(
-    <></>
+    <Forms />
   );
 }
 
---
+++ src/componentes/Forms.jsx
@@ -0,0 +1,7 @@
+const Forms = () => {
+    return (
+        <h2>Teste</h2>
+    );
+
+};
+export default Forms 
