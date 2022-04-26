f01754df69395149be196c1c39dec294fa26721b
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-04-25 22:35:10 -0300

    Componente exemplo criado e sincronizado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import Menu from './componentes/Menu';
 
 function App() {
   return(
-    <></>
+    <Menu />
   );
 }
 
---
+++ src/componentes/Menu.jsx
@@ -0,0 +1,7 @@
+const Menu = () => {
+    return(
+        <h1>Menu</h1>
+    )
+}
+
+export default Menu
