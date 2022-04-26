991a26ad392225a2823e983f62d6b8aec026ed05
Carlos-Jr1 <junior_kd2012@hotmail.com>
2022-04-26 00:32:23 -0300

    Roda-Pé

--- src/componentes/Forms.jsx
+++ src/componentes/Forms.jsx
@@ -1,7 +1,27 @@
-const Forms = () => {
-    return (
-        <h2>Teste</h2>
-    );
+import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
 
+const Forms = () => {
+  return (
+    <div>
+      <h2>SDJD</h2>
+      <input placeholder="E-mail"></input>
+      <button>Inscrever</button>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            {/*<MenuIcon />*/}
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            Photos
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
-export default Forms 
+export default Forms;
