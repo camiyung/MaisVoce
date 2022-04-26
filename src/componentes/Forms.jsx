f771d0b808c65eeabd099cc171dde70121954009
Carlos-Jr1 <junior_kd2012@hotmail.com>
2022-04-26 12:44:17 -0300

    organizei-fooder

--- src/componentes/Forms.jsx
+++ src/componentes/Forms.jsx
@@ -1,30 +1,54 @@
 import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+
+import "./estilos/Forms.css";
 const Forms = () => {
   return (
-    <div>
-      <h2>SDJD</h2>
-      <TextField id="outlined-basic" label="E-mail" variant="outlined" />
-      <Button variant="contained" size="large">
+    <div >
+      <Typography variant="h4" align="center" mb={3} mt={2}>
+        Assinnante do MaisVocê receba receitas diariamente e promoçoes exclusiva.
+      </Typography>
+      <div class="formulario">
+        <TextField
+          id="outlined-basic"
+          label="E-mail"
+          variant="outlined"
+          size="small"
+        />
+        <Button variant="contained" size="large">
           Inscrever
         </Button>
+      </div>
+
       <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton
-            edge="start"
+        <Toolbar className="rodape" variant="dense">
+          <Typography
+            className="rodape-esquerdo"
+            variant="h6"
             color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+            component="div"
           >
-            {/*<MenuIcon />*/}
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            Photos
+            MaisVocê
           </Typography>
+          <IconButton color="inherit" href="#">
+            <FacebookIcon fontSize="large" />
+          </IconButton>
+          <IconButton color="inherit" href="#">
+            <InstagramIcon fontSize="large" />
+          </IconButton>
+          <IconButton color="inherit" href="#">
+            <TwitterIcon fontSize="large" />
+          </IconButton>
+          <IconButton color="inherit" href="#">
+            <YouTubeIcon fontSize="large" />
+          </IconButton>
         </Toolbar>
       </AppBar>
-      
     </div>
   );
 };
