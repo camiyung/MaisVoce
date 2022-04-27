293a470e75625da9c923de6326d7895d8d4d85d2
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-04-26 21:18:51 -0300

    Novo component teste criado, esboço pronto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-import Menu from './componentes/Menu';
+import Menu2 from './componentes/Menu2-gaveta';
 
 function App() {
   return(
-    <Menu />
+    <>
+      <Menu2 />
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat deleniti libero reprehenderit suscipit velit soluta quos nihil debitis natus dolorem delectus, ut nulla eum maxime distinctio repudiandae sit. Mollitia, unde.</h1>
+      <h1>Illo vitae quas vero obcaecati nulla hic officiis quidem quis, doloribus odio eveniet architecto. Dolor culpa odit corrupti ratione vero quae, ut voluptatem. Sit libero placeat eaque, alias pariatur ab.</h1>
+      <h1>Laborum delectus aliquid quod beatae omnis. Unde qui facilis minus accusantium fugit consectetur neque quos velit sed iusto magnam minima, quasi quisquam quidem, harum est blanditiis? Earum itaque aliquid sit.</h1>
+      <h1>Alias exercitationem modi perspiciatis earum ea debitis explicabo, est magni! Autem reprehenderit reiciendis mollitia? Repellendus, laudantium necessitatibus beatae alias facilis quisquam rerum itaque totam dicta deserunt tempore distinctio, esse porro.</h1>
+      <h1>Accusantium at ratione harum minima facere quo, deserunt similique, nobis minus quasi quod quidem consequuntur magnam voluptates repudiandae? A, iusto recusandae. Autem eum odio fugit animi corporis. Iusto, nemo voluptates.</h1>
+      <h1>Alias exercitationem modi perspiciatis earum ea debitis explicabo, est magni! Autem reprehenderit reiciendis mollitia? Repellendus, laudantium necessitatibus beatae alias facilis quisquam rerum itaque totam dicta deserunt tempore distinctio, esse porro.</h1>
+      <h1>Alias exercitationem modi perspiciatis earum ea debitis explicabo, est magni! Autem reprehenderit reiciendis mollitia? Repellendus, laudantium necessitatibus beatae alias facilis quisquam rerum itaque totam dicta deserunt tempore distinctio, esse porro.</h1>
+      <h1>Alias exercitationem modi perspiciatis earum ea debitis explicabo, est magni! Autem reprehenderit reiciendis mollitia? Repellendus, laudantium necessitatibus beatae alias facilis quisquam rerum itaque totam dicta deserunt tempore distinctio, esse porro.</h1>
+      <h1>Accusantium at ratione harum minima facere quo, deserunt similique, nobis minus quasi quod quidem consequuntur magnam voluptates repudiandae? A, iusto recusandae. Autem eum odio fugit animi corporis. Iusto, nemo voluptates.</h1>
+      <h1>Accusantium at ratione harum minima facere quo, deserunt similique, nobis minus quasi quod quidem consequuntur magnam voluptates repudiandae? A, iusto recusandae. Autem eum odio fugit animi corporis. Iusto, nemo voluptates.</h1>
+      <h1>Accusantium at ratione harum minima facere quo, deserunt similique, nobis minus quasi quod quidem consequuntur magnam voluptates repudiandae? A, iusto recusandae. Autem eum odio fugit animi corporis. Iusto, nemo voluptates.</h1>
+    </>
   );
 }
 
---
+++ src/componentes/Menu2-gaveta.jsx
@@ -0,0 +1,207 @@
+import * as React from 'react';
+
+import './estilos/Menu2-gaveta.css'
+
+import { styled, useTheme, alpha } from '@mui/material/styles';
+
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+import HomeIcon from '@mui/icons-material/Home';
+import AnnouncementIcon from '@mui/icons-material/Announcement';
+import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
+import PeopleIcon from '@mui/icons-material/People';
+import InfoIcon from '@mui/icons-material/Info';
+import ForumIcon from '@mui/icons-material/Forum';
+import CampaignIcon from '@mui/icons-material/Campaign';
+
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+// Barra de search
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+// FIM DA BARRA DE SEARCH
+
+export default function PersistentDrawerLeft() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+ const listaDeIcones = [<HomeIcon />, <AnnouncementIcon />, <ReceiptLongIcon />, <HistoryEduIcon />, <PeopleIcon />, <InfoIcon />, <ForumIcon />, <CampaignIcon />]
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Persistent drawer
+          </Typography>
+          <Search  className='procurar'>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Procurar..."
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Home', 'Novidades', 'Receitas', 'Reviews', 'Parceiros', 'Sobre', 'Fale conosco', 'Iniciativas'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {listaDeIcones[index]}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        <Typography paragraph></Typography>
+        <Typography paragraph></Typography>
+      </Main>
+    </Box>
+  );
+}
