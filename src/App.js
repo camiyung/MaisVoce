8a663a5a021959a6a41cd74c2184b0a5a68d7f71
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-04-27 16:44:11 -0300

    Centralização do arquivo menu.jsx com estilo ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Menu2 from './componentes/Menu2-gaveta';
-// import Menu from './componentes/Menu'
+import Menu from './componentes/Menu'
 
 function App() {
   return(
     <>
-      <Menu2 />
-      {/* <Menu /> */}
+      <Menu />
       <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.</h1>
     </>
   );
--- src/componentes/Menu.jsx
+++ src/componentes/Menu.jsx
@@ -1,12 +1,83 @@
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
+import * as React from 'react';
+
+import './estilos/Menu.css'
+
+import { styled, useTheme, alpha } from '@mui/material/styles';
+
 import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
+
+import HomeIcon from '@mui/icons-material/Home';
+import AnnouncementIcon from '@mui/icons-material/Announcement';
+import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
+import PeopleIcon from '@mui/icons-material/People';
+import InfoIcon from '@mui/icons-material/Info';
+import ForumIcon from '@mui/icons-material/Forum';
+import CampaignIcon from '@mui/icons-material/Campaign';
+
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -50,39 +121,85 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-export default function SearchAppBar() {
+export default function PersistentDrawerLeft() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+ const listaDeIcones = [<HomeIcon />, <AnnouncementIcon />, <ReceiptLongIcon />, <HistoryEduIcon />, <PeopleIcon />, <InfoIcon />, <ForumIcon />, <CampaignIcon />]
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="sticky">
-        <Toolbar>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar className='menu-barra'>
           <IconButton
-            size="large"
-            edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{ mr: 2 }}
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            MaisVocê - Receitas Brasileiras
+          <Typography variant="h4" noWrap component="div">
+            MaisVocê
           </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Procurar..."
-              inputProps={{ 'aria-label': 'search' }}
-            />
+          {/* <div className='campo-busca'> */}
+            <Search >
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Procurar..."
+                inputProps={{ 'aria-label': 'search' }}
+              />
           </Search>
+          {/* </div> */}
         </Toolbar>
       </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Home', 'Novidades', 'Receitas', 'Reviews', 'Parceiros', 'Sobre', 'Fale conosco', 'Iniciativas'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {listaDeIcones[index]}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      {/* <Main open={open}> */}
+        <DrawerHeader />
+        {/* <Typography paragraph></Typography> */}
+        {/* <Typography paragraph></Typography> */}
+      {/* </Main> */}
     </Box>
   );
 }
