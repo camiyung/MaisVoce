1e3f44448e1a2e7b0ffe105746eceefd4a0915d9
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-04-28 17:09:04 -0300

    Alterei o texto no app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ function App() {
   return(
     <>
       <Menu />
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio dolori.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.</h1>
     </>
   );
 }
