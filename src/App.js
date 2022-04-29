f8323a599c38167eb5fe809184fc2bd12c505823
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-04-28 23:32:05 -0300

    Possível versao final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Menu from './componentes/Menu'
+import louroJose from './assets/logo-recortado.png'
 
 function App() {
   return(
     <>
-      <Menu />
+      <Menu imagem={louroJose}/>
       <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio dolori.
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
