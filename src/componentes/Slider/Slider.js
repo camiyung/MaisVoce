3e3069bee6a7b376e25d1c12c1f21ed5abc78bfd
Camila Yung Alves <camilayung92@gmail.com>
2022-04-30 14:33:51 -0300

    Adicionar alt nas imagens

--- src/componentes/Slider/Slider.js
+++ src/componentes/Slider/Slider.js
@@ -38,7 +38,7 @@ export default function Slider() {
                     className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
                     >
                         <img 
-                        src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`} 
+                        src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`} alt="Slides" 
                         />
                     </div>
                 )
