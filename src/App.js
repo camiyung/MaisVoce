90945ba6593b0d24c91ed30c3a955d8733879693
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-04-30 14:53:02 -0300

    Commit final da branch menu-header

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ function App() {
   return(
     <>
       <Menu />
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio dolori.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quam eaque blanditiis est unde in saepe voluptates facere. Vitae, quam! Harum, veritatis reprehenderit quae alias laudantium repellendus sint optio doloribus.</h1>
     </>
   );
 }
